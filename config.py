7a46aaeba555bd1e622e9021d83bd3364531100a
muki292 <mukil3826@gmail.com>
2026-04-19 13:24:39 +0530

    inserted

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-MONGO_URI = "mongodb+srv://mukil3826_db_user:<password>@cluster0.9pampd2.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://mukil3826_db_user:<mukil@123>@cluster0.9pampd2.mongodb.net/?appName=Cluster0"
